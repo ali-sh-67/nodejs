b09dfcf903e0b2acb3acfef6888bc2650fe469c9
ali-sh-67 <ali.shahbeaki@gmail.com>
2022-07-21 16:14:22 +0430

    create base

---
+++ src/routes/auth/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./controller');
+
+router.post(
+    '/register',
+     controller.register
+);
+router.post(
+    '/login',
+     controller.login
+);
+ 
+
+
+module.exports=router;
